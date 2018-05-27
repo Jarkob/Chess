874ff75761a42700826f4bdda0205ea561e4b460
Jakob Bussas <jakobbu@gmx.de>
2018-05-27 12:08:03 +0200

    comments

--- src/game/App.java
+++ src/game/App.java
@@ -5,6 +5,14 @@ import models.Game;
 public class App
 {
 	public static void main(String[] args) {
+		// TODO:
+		// en passant
+		// trade pawns
+		// rochade
+		// check mate
+		// move only legal if king isn't threatened afterwards
+		
+		// primitive implementation: game ends when king is taken
 		Game testGame = new Game(1);
 		System.out.printf(testGame.getBoard().toString());
 	}
