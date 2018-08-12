9e3fecb98076413f28989226a38cc5042641134e
Jakob Bussas <jakobbu@gmx.de>
2018-08-12 11:49:43 +0200

    fixed bug in which king could not move diagonally

--- src/pieces/King.java
+++ src/pieces/King.java
@@ -23,7 +23,7 @@ public class King extends Piece
 	@Override
 	public boolean isMoveLegal(Move move)
 	{
-		if((Math.abs((int) move.getOldTile().getX() - (int) move.getNewTile().getX()) == 1) && (Math.abs(move.getOldTile().getY() - move.getNewTile().getY()) == 1)) {
+		if((Math.abs((int) move.getOldTile().getX() - (int) move.getNewTile().getX()) == 1) || (Math.abs(move.getOldTile().getY() - move.getNewTile().getY()) == 1)) {
 			// TODO: is tile threatened
 			return move.isLegal();
 		}
